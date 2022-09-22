757633df3dbc0f2285bd2fa325d6b1309c1bd0da
Daniella Furquim <daniella.lorenzano@gmail.com>
2022-09-22 07:50:25 -0300

    atualização da api do get e login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,7 @@
 const UserSchema = require("../models/userSchema");
 
 const getAll = async (req, res) => {
-  console.log("User All");
+  console.log("Get All");
   UserSchema.find(function (err, users) {
     if(err) {
       res.status(500).send({ message: err.message })
@@ -9,7 +9,28 @@ const getAll = async (req, res) => {
       res.status(200).send(users)
   }) 
 };
-
+const getUser = async (req, res) => {
+  console.log("Get User");
+  UserSchema.findById(req.params.id,function (err, users) {
+    console.log (req.params.id);
+    if(err) {
+      res.status(500).send({ message: err.message })
+    }
+      res.status(200).send(users)
+  }) 
+};
+const getLogin = async (req, res) => {
+  console.log("Get Login");
+  UserSchema.findById(req.params.id,function (err, users) {
+    console.log (req.params.id);
+    if(err) {
+      res.status(500).send({ message: err.message })
+    }
+      res.status(200).send(users)
+  }) 
+};
 module.exports = {
-  getAll
+  getAll, 
+  getUser,
+  getLogin
 };
